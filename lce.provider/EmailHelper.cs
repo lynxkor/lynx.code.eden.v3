d1618522f38e844185bfbf673b8f05559bb30f24
山猫 <lynx.kor@163.com>
2019-06-06 10:44:55 +0800

    EmailHelper

---
+++ lce.provider/EmailHelper.cs
@@ -0,0 +1,86 @@
+﻿// action：
+// file name：${namespace}.EmailHelper.cs
+// author：lynx lynx.kor@163.com @ 2019/6/5 23:15
+// copyright (c) 2019 lynxce.com
+// desc：
+// > add description for EmailHelper
+// revision：
+//
+using System;
+namespace lce.provider
+{
+    public static class EmailHelper
+    {
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="subject"></param>
+        /// <param name="mailto"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="smtp"></param>
+        public static void Send(string content, string subject, string mailto, string username, string password, string smtp)
+        {
+            var uri = new Uri(smtp);
+            var host = uri.Host.ToLower().Replace("www.", "");
+            var mailfrom = string.Format("{0}@{1}", username, host);
+            Send(content, subject, mailto, username, password, mailfrom, smtp);
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="subject"></param>
+        /// <param name="mailto"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="mailfrom"></param>
+        /// <param name="smtp"></param>
+        /// <param name="port"></param>
+        public static void Send(
+            string content,
+            string subject,
+            string mailto = "lynx.kor@163.com",
+            string username = "lynxce",
+            string password = "134567890",
+            string mailfrom = "lynxce@163.com",
+            string smtp = "smtp.163.com",
+            int port = 25)
+        {
+            try
+            {
+                //设置用户名和密码。
+                //用户登陆信息
+                var myCredentials = new System.Net.NetworkCredential(username, password);
+                //确定smtp服务器地址。实例化一个Smtp客户端
+                var client = new System.Net.Mail.SmtpClient(smtp, port)
+                {
+                    DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network,
+                    Credentials = myCredentials
+                };
+                //构造一个发件人地址对象
+                var mfrom = new System.Net.Mail.MailAddress(mailfrom, mailfrom, System.Text.Encoding.UTF8);
+                //构造一个收件人地址对象
+                var mto = new System.Net.Mail.MailAddress(mailto, mailto, System.Text.Encoding.UTF8);
+                //构造一个Email的Message对象
+                var mailMessage = new System.Net.Mail.MailMessage(mfrom, mto)
+                {
+                    //添加邮件主题和内容
+                    SubjectEncoding = System.Text.Encoding.UTF8,
+                    Subject = subject,
+                    BodyEncoding = System.Text.Encoding.UTF8,
+                    Body = content,
+                    IsBodyHtml = false
+                };
+                //发送邮件
+                client.Send(mailMessage);
+            }
+            catch
+            {
+                Console.WriteLine("send mail err");
+            }
+        }
+    }
+}

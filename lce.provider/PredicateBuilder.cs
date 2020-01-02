00da8852f0fae33eb68932ed346dd3cae47e9eb3
山猫 <lynx.kor@163.com>
2020-01-03 00:09:45 +0800

    注释增加使用说明

--- lce.provider/PredicateBuilder.cs
+++ lce.provider/PredicateBuilder.cs
@@ -1,8 +1,12 @@
-﻿// action：
-// file name：${namespace}.PredicateBuilder.cs
+﻿// action：lambda PredicateBuilder
+// file name：lce.provider.PredicateBuilder.cs
 // author：lynx lynx.kor@163.com @ 2019/6/5 23:01
 // copyright (c) 2019 lynxce.com
-// desc：
+// desc：what can you use this.
+// var where = PredicateBuilder.True<model>();
+// or where = PredicateBuilder.False<model>();
+// where = where.And(x=>x.feild1 == value1);
+// where = where.Or(x=>x.feild2 == value2);
 // > add description for PredicateBuilder
 // revision：
 //
@@ -23,12 +27,14 @@ namespace lce.provider
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         public static Expression<Func<T, bool>> True<T>() { return f => true; }
+
         /// <summary>
         /// False
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         public static Expression<Func<T, bool>> False<T>() { return f => false; }
+
         /// <summary>
         /// Left And Right
         /// </summary>
@@ -41,6 +47,7 @@ namespace lce.provider
             var invoked = Expression.Invoke(right, left.Parameters.Cast<Expression>());
             return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(left.Body, invoked), left.Parameters);
         }
+
         /// <summary>
         /// Left Or Right
         /// </summary>

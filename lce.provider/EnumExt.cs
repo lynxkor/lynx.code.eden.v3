ee1b2b5253b5a4908a82af6c80e4e4048800eba5
山猫 <lynx.kor@163.com>
2020-03-08 00:14:42 +0800

    enum.list

--- lce.provider/EnumExt.cs
+++ lce.provider/EnumExt.cs
@@ -7,6 +7,7 @@
 // revision：
 //
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
@@ -26,10 +27,29 @@ namespace lce.provider
         public static string Description(this Enum eValue)
         {
             var typeInfo = eValue.GetType().GetTypeInfo();
-            MemberInfo memberInfo = typeInfo.GetMember(eValue.ToString()).First();
+            var memberInfo = typeInfo.GetMember(eValue.ToString()).First();
             var descriptionAttribute =
               memberInfo.GetCustomAttribute<DescriptionAttribute>();
             return descriptionAttribute.Description;
+        }
+
+        /// <summary>
+        /// 获取枚举类型的数据字典
+        /// </summary>
+        /// <param name="enum">枚举类型</param>
+        /// <returns></returns>
+        public static Dictionary<string, int> List<T>()
+        {
+            var type = typeof(T);
+            var list = new Dictionary<string, int>();
+
+            foreach (var e in Enum.GetValues(type))
+            {
+                var desc = type.GetMember(e.ToString()).First().GetCustomAttribute<DescriptionAttribute>().Description;
+                list.Add(desc, Convert.ToInt32(e));
+            }
+            return list;
+
         }
     }
 }

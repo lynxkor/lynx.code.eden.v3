2a7e634d2ade432e03baae7c1ce2843ef3295b8d
山猫 <lynx.kor@163.com>
2020-03-14 23:18:12 +0800

    ServiceProviderAttribute

---
+++ lce.provider/Attributes/ServiceRegisterAttribute.cs
@@ -0,0 +1,41 @@
+﻿// /* file name：${namespace}.ServiceProviderAttribute.cs
+// * author：lynx <lynx.kor@163.com> @ 2020/3/14 20:23
+// * copyright (c) 2020 Copyright@lynxce.com
+// * desc：
+// * > add description for ServiceProviderAttribute
+// * revision：
+// *
+// */
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace lce.provider.Attributes
+{
+    /// <summary>
+    /// 用于标记服务，便于注入注册
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class ServiceProviderAttribute : Attribute
+    {
+        /// <summary>
+        /// 生命周期，默认为Transient
+        /// </summary>
+        public ServiceLifetime Lifetime { get; set; }
+
+        /// <summary>
+        /// 服务类型，用于映射
+        /// </summary>
+        public Type Type { get; set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="type">服务类型</param>
+        /// <param name="lifetime">生命周期</param>
+        public ServiceProviderAttribute(Type type, ServiceLifetime lifetime = ServiceLifetime.Transient)
+        {
+            Type = type;
+            Lifetime = lifetime;
+        }
+    }
+}

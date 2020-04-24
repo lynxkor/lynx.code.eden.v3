75d2d55ace217f84c9eb6bedf24399958ae7ccf3
山猫 <lynx.kor@163.com>
2020-04-25 00:05:36 +0800

    Condition

--- lce.provider/QuerySqlExt.cs
+++ lce.provider/QuerySqlExt.cs
@@ -7,6 +7,9 @@
 *
 */
 
+using System.Collections.Generic;
+using System.Linq;
+
 namespace lce.provider
 {
     /// <summary>
@@ -14,5 +17,60 @@ namespace lce.provider
     /// </summary>
     public static class QuerySqlExt
     {
+        /// <summary>
+        /// 拼接查询条件
+        /// </summary>
+        /// <param name="name"> </param>
+        /// <param name="value"></param>
+        /// <returns>AND {name} = {value}</returns>
+        public static object Condition(string name, int? value)
+        {
+            if (!value.HasValue || value.Value == -1) return "";
+            return $@"AND {name} = {value} ";
+        }
+
+        /// <summary>
+        /// 拼接查询条件
+        /// </summary>
+        /// <param name="name"> </param>
+        /// <param name="value"></param>
+        /// <returns>AND {name} = '{value}'</returns>
+        public static object Condition(string name, string value)
+        {
+            if (string.IsNullOrEmpty(value)) return "";
+            return $@"AND {name} = '{value}' ";
+        }
+
+        /// <summary>
+        /// 拼接查询条件
+        /// </summary>
+        /// <param name="name">  </param>
+        /// <param name="values"></param>
+        /// <returns>AND {name} = {values}/IN({values})</returns>
+        public static string Condition(string name, IList<int> values)
+        {
+            if (null == values || values.Count == 0) return "";
+            if (values.Count == 1)
+                return $@"AND {name} = {values[0]} ";
+            else
+                return $@"AND {name} IN({string.Join(",", values)}) ";
+        }
+
+        /// <summary>
+        /// 拼接查询条件
+        /// </summary>
+        /// <param name="name">  </param>
+        /// <param name="values"></param>
+        /// <returns>AND {name} = '{values}'/IN('{values}')</returns>
+        public static string Condition(string name, IList<string> values)
+        {
+            if (null == values || values.Count == 0) return "";
+            if (values.Count == 1)
+                return $@"AND {name} = '{values[0]}' ";
+            else
+                return $@"AND {name} IN({string.Join(",", values.Select(x => $@"'{x}'"))}) ";
+        }
+
+
     }
 }

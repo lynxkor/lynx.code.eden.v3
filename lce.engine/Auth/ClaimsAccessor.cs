56ce95c93f2ac033143d8274dcef8109f9df1642
lynxkor <lynx.kor@163.com>
2020-08-17 17:28:32 +0800

    lce.engine.auth

---
+++ lce.engine/Auth/ClaimsAccessor.cs
@@ -0,0 +1,40 @@
+﻿/* file name：${namespace}.ClaimsAccessor.cs
+* author：lynx <lynx.kor@163.com> @ 2020/3/15 11:19
+* copyright (c) 2020 Copyright@lynxce.com
+* desc：
+* > add description for ClaimsAccessor
+* revision：
+*
+*/
+
+using System.Linq;
+using System.Security.Claims;
+
+namespace lce.provider.Auth
+{
+    /// <summary>
+    /// </summary>
+    public class ClaimsAccessor : IClaimsAccessor
+    {
+        /// <summary>
+        /// </summary>
+        protected IPrincipalAccessor _principalAccessor { get; }
+
+        /// <summary>
+        /// </summary>
+        public ClaimsAccessor(IPrincipalAccessor principalAccessor)
+        {
+            _principalAccessor = principalAccessor;
+            var userJson = principalAccessor.Principal?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.UserData)?.Value;
+
+            if (!string.IsNullOrEmpty(userJson))
+            {
+                CurrentUser = userJson.ToModel<CurrentUser>();
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        public IUser CurrentUser { get; }
+    }
+}
---
+++ lce.engine/Auth/IUser.cs
@@ -0,0 +1,59 @@
+﻿/* file name：${namespace}.IUser.cs
+* author：lynx <lynx.kor@163.com> @ 2020/3/14 13:14
+* copyright (c) 2020 Copyright@lynxce.com
+* desc：
+* > add description for IUser
+* revision：
+*
+*/
+
+using System.Collections.Generic;
+
+namespace lce.provider.Auth
+{
+    /// <summary>
+    /// 系统用户基类
+    /// </summary>
+    public interface IUser
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        int Id { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        string UserName { get; set; }
+
+        /// <summary>
+        /// 组织Id
+        /// </summary>
+        int OrganId { get; set; }
+
+        /// <summary>
+        /// 组织名称
+        /// </summary>
+        string OrganName { get; set; }
+
+        /// <summary>
+        /// 公司Id
+        /// </summary>
+        int CompId { get; set; }
+
+        /// <summary>
+        /// 公司名称
+        /// </summary>
+        string CompName { get; set; }
+
+        /// <summary>
+        /// 角色
+        /// </summary>
+        IList<string> RoleIds { get; set; }
+    }
+}

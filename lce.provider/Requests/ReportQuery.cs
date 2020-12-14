7c87f79c40cc69cec66fe790b62502e5da496faf
lynxkor <lynx.kor@163.com>
2020-12-14 19:46:40 +0800

    ReportQuery + PageQuery

--- lce.provider/Requests/ReportQuery.cs
+++ lce.provider/Requests/ReportQuery.cs
@@ -19,17 +19,17 @@ namespace lce.provider.Requests
     /// <para>时间维度为1010~1050时取Date[0]计算对应日期所在的时间维度第一天到最后一天;</para>
     /// <para>时间维度为1060时根据Date中两个日期时间值指定区间用作运算</para>
     /// </summary>
-    public class ReportQuery
+    public class ReportQuery : PageQuery
     {
+        /// <summary>
+        /// 日期时间
+        /// </summary>
+        public IList<DateTime> Date { get; set; } = new List<DateTime> { };
+
         /// <summary>
         /// 时间维度 1010:年，1020:月，1030:日，1040:周，1050:季，1060:自定义区间
         /// <code>DateTimeExt.DateType</code>
         /// </summary>
         public DateType DateType { get; set; } = DateType.Month;
-
-        /// <summary>
-        /// 日期时间
-        /// </summary>
-        public IList<DateTime> Date { get; set; } = new List<DateTime> { };
     }
 }

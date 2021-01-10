f154a71a49974229ce2868d16ff5a578bb29ae7b
lynxkor <lynx.kor@163.com>
2021-01-10 10:40:53 +0800

    add weekinmonth

--- lce.provider/DateTimeExt.cs
+++ lce.provider/DateTimeExt.cs
@@ -18,50 +18,6 @@ namespace lce.provider
     public static class DateTimeExt
     {
         /// <summary>
-        /// string 2 datetime
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static DateTime ToDateTime(this object input)
-        {
-            try
-            {
-                return Convert.ToDateTime(input);
-            }
-            catch
-            {
-                return DateTime.MinValue;
-            }
-        }
-
-        /// <summary>
-        /// string 2 utc
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static DateTime ToUTC(this object input)
-        {
-            try
-            {
-                return Convert.ToDateTime(input).ToUniversalTime();
-            }
-            catch
-            {
-                return DateTime.MinValue;
-            }
-        }
-
-        /// <summary>
-        /// Format yyyy-MM-dd
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static string FormatShort(this DateTime input)
-        {
-            return input.Format("yyyy-MM-dd");
-        }
-
-        /// <summary>
         /// Format datetime,default yyyy-MM-dd HH:mm:ss
         /// </summary>
         /// <param name="input"> </param>
@@ -80,21 +36,13 @@ namespace lce.provider
         }
 
         /// <summary>
-        /// Format datetime 4 code
+        /// Format yyyy-MM-dd
         /// </summary>
-        /// <param name="input"> </param>
-        /// <param name="format"></param>
+        /// <param name="input"></param>
         /// <returns></returns>
-        public static string ToCode(this DateTime input, string format = "yyyyMMddHHmmss")
+        public static string FormatShort(this DateTime input)
         {
-            try
-            {
-                return input.ToString(format);
-            }
-            catch
-            {
-                return "参数有误";
-            }
+            return input.Format("yyyy-MM-dd");
         }
 
         /// <summary>
@@ -118,46 +66,13 @@ namespace lce.provider
         }
 
         /// <summary>
-        /// 取得某年的第一天
+        /// 季度
         /// </summary>
         /// <param name="dateTime"></param>
         /// <returns></returns>
-        public static DateTime YearFirstDay(this DateTime dateTime)
+        public static int Quarter(this DateTime dateTime)
         {
-            return dateTime.AddMonths(1 - dateTime.Month).AddDays(1 - dateTime.Day).Date;
-        }
-
-        /// <summary>
-        /// 取得某年的最后一天
-        /// </summary>
-        /// <param name="dateTime"></param>
-        /// <returns></returns>
-        public static DateTime YearLastDay(this DateTime dateTime)
-        {
-            //次年第一个月
-            dateTime = dateTime.AddMonths(1 - dateTime.Month).Date.AddYears(1);
-            //月度第一天 00:00 减一分钟=当年最后一分钟最后一秒
-            return dateTime.AddDays(1 - dateTime.Day).Date.AddSeconds(-1);
-        }
-
-        /// <summary>
-        /// 取得本周第一天（周日为第一天）
-        /// </summary>
-        /// <param name="dateTime"></param>
-        /// <returns></returns>
-        public static DateTime WeekFirstDay(this DateTime dateTime)
-        {
-            return dateTime.AddDays(0 - (int)dateTime.DayOfWeek).Date;
-        }
-
-        /// <summary>
-        /// 得到本周最后一天（周六为最后一天）
-        /// </summary>
-        /// <param name="dateTime"></param>
-        /// <returns></returns>
-        public static DateTime WeekLastDay(this DateTime dateTime)
-        {
-            return dateTime.AddDays(7 - (int)dateTime.DayOfWeek).Date.AddSeconds(-1);
+            return dateTime.Month / 3 + 1;
         }
 
         /// <summary>
@@ -178,16 +93,6 @@ namespace lce.provider
         public static DateTime QuarterLastDay(this DateTime dateTime)
         {
             return dateTime.AddMonths(3 - ((dateTime.Month - 1) % 3)).Date.AddSeconds(-1); //最后一秒
-        }
-
-        /// <summary>
-        /// 季度
-        /// </summary>
-        /// <param name="dateTime"></param>
-        /// <returns></returns>
-        public static int Quarter(this DateTime dateTime)
-        {
-            return dateTime.Month / 3 + 1;
         }
 
         /// <summary>
@@ -227,6 +132,102 @@ namespace lce.provider
                     endDate = startDate.AddDays(1).AddSeconds(-1);
                     break;
             }
+        }
+
+        /// <summary>
+        /// Format datetime 4 code
+        /// </summary>
+        /// <param name="input"> </param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public static string ToCode(this DateTime input, string format = "yyyyMMddHHmmss")
+        {
+            try
+            {
+                return input.ToString(format);
+            }
+            catch
+            {
+                return "参数有误";
+            }
+        }
+
+        /// <summary>
+        /// string 2 datetime
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this object input)
+        {
+            try
+            {
+                return Convert.ToDateTime(input);
+            }
+            catch
+            {
+                return DateTime.MinValue;
+            }
+        }
+
+        /// <summary>
+        /// string 2 utc
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static DateTime ToUTC(this object input)
+        {
+            try
+            {
+                return Convert.ToDateTime(input).ToUniversalTime();
+            }
+            catch
+            {
+                return DateTime.MinValue;
+            }
+        }
+
+        /// <summary>
+        /// 取得本周第一天（周日为第一天）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime WeekFirstDay(this DateTime dateTime)
+        {
+            return dateTime.AddDays(0 - (int)dateTime.DayOfWeek).Date;
+        }
+
+        /// <summary>
+        /// 得到本周最后一天（周六为最后一天）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime WeekLastDay(this DateTime dateTime)
+        {
+            return dateTime.AddDays(7 - (int)dateTime.DayOfWeek).Date.AddSeconds(-1);
+        }
+
+        /// <summary>
+        /// 取得某年的第一天
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime YearFirstDay(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, 1, 1, 0, 0, 0);
+        }
+
+        /// <summary>
+        /// 取得某年的最后一天
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime YearLastDay(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year + 1, 1, 1, 0, 0, 0).AddSeconds(-1);
+            ////次年第一个月
+            //dateTime = dateTime.AddMonths(1 - dateTime.Month).Date.AddYears(1);
+            ////月度第一天 00:00 减一分钟=当年最后一分钟最后一秒
+            //return dateTime.AddDays(1 - dateTime.Day).Date.AddSeconds(-1);
         }
     }
 }
